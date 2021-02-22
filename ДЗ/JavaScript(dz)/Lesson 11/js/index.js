9e1ac06ca2bb1c2a0cf3e5e576a17a38b90690f3
tikurila <karinapopova20044002pop@gmail.com>
2021-02-22 16:26:59 +0200

    dz-js-lesson11

---
+++ ДЗ/JavaScript(dz)/Lesson 11/js/index.js
@@ -0,0 +1,16 @@
+let name = prompt("Введите имя: ");
+let age = prompt("Введите возраст: ")
+
+if(age > 22) 
+alert('Welcome, ' + name)
+if (age < 18)
+    alert('You are not allowed to visit this website');
+if (age = 18, 19, 20, 21, 22)
+var button;
+var question = confirm("Are you sure you want to continue?");
+if (question == true) {
+  button = alert ("Welcome, " + name);
+} else {
+  button = alert ("You are not allowed to visit this website");
+};
+
