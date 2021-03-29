9492cbc007e31017df120b1851de96f4a27f89ee
tikurila <karinapopova20044002pop@gmail.com>
2021-03-29 20:48:25 +0300

    dz-js-lesson21

--- ДЗ/JavaScript(dz)/Lesson 21/js/index.js
+++ ДЗ/JavaScript(dz)/Lesson 21/js/index.js
@@ -1,28 +1,57 @@
-const view = `<i class="fas fa-eye-slash icon-password view"></i>`
-function show_hide_password(target) {
-  let input = document.getElementById("password-input");
-  if (input.getAttribute("type") == "password") {
-    target.classList.add("view");
+let eyeClose = document.getElementById("hide-icon");
+let input = document.getElementById("password-input");
+let inputComfirm = document.getElementById("confirm_password");
+let eyeCloseConfirm = document.getElementById("hide-icon2");
+let error = document.getElementById("error");
+error.textContent = "";
+// модальное окно
+let modal = document.getElementById("my_modal");
+let span = document.getElementsByClassName("close_modal_window")[0];
+let closemodal = document.getElementById("close");
 
+function password() {
+  if (input.getAttribute("type") == "password") {
     input.setAttribute("type", "text");
+    eyeClose.classList.add("hide-icon");
   } else {
-    // target.classList.remove("view");
+    eyeClose.classList.remove("hide-icon");
     input.setAttribute("type", "password");
   }
-  return false;
 }
 
-
-function show_hide_password2(target) {
-	let input = document.getElementById("password-input2");
-	if (input.getAttribute("type") == "password") {
-	//   target.classList.add("view");
-	  input.setAttribute("type", "text");
-	} else {
-	//   target.classList.remove("view");
-	  input.setAttribute("type", "password");
-	}
-	return false;
+function confirm_password() {
+  if (inputComfirm.getAttribute("type") == "password") {
+    inputComfirm.setAttribute("type", "text");
+    eyeCloseConfirm.classList.add("hide-icon");
+  } else {
+    eyeCloseConfirm.classList.remove("hide-icon");
+    inputComfirm.setAttribute("type", "password");
   }
+}
 
-  
+function password_compare() {
+  if (
+    input.value == inputComfirm.value &&
+    input.value != "" &&
+    inputComfirm.value != ""
+  ) {
+    error.classList.remove("error");
+    modal.style.display = "block";
+    closemodal.style.display = "block";
+    clearinput();
+    error.classList.add("hide-icon");
+  } else if (input.value != inputComfirm.value) {
+    error.classList.remove("hide-icon");
+    error.innerHTML = "Нужно ввести одинаковые значения";
+    error.classList.add("error");
+  }
+}
+
+function closewindow() {
+  closemodal.style.display = "none";
+}
+
+function clearinput() {
+  input.value = "";
+  inputComfirm.value = "";
+}
